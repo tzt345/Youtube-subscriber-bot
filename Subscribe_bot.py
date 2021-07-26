15bd5078800ba96f602a7f03611d398374ed463c
tzt345 <83703996+tzt345@users.noreply.github.com>
2021-07-26 19:19:35 +0800

    Update Subscribe_bot.py

--- Subscribe_bot.py
+++ Subscribe_bot.py
@@ -22,7 +22,7 @@ class SubBot:
     bellButtonClick = "Bell[1].click();"    
 
     # Type here your channel url:
-    url = input("Enter your channel link = ")
+    url = input("https://youtube.com/c/RXNationGaming = ")
     
     listOfBrowser = ['start chrome '+url,'start firefox '+url]
 
